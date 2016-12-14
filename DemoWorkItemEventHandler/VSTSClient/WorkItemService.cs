df1c31e23687ab9c1d1f34ae6cb6f36f135f8765
Marc Müller <mmueller@4tecture.ch>
2016-12-14 21:43:46 +0100

    Initial Commit for Demo

---
+++ DemoWorkItemEventHandler/VSTSClient/WorkItemService.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DemoWorkItemEventHandler.VSTSClient
+{
+    public class WorkItemService : IDisposable
+    {
+        private VssConnection connection;
+        private Guid projectId;
+        private WorkItemTrackingHttpClient witClient;
+
+        public WorkItemService(string collectionUri, Guid projectId, string pat)
+        {
+            connection = new VssConnection(new Uri(collectionUri), new VssBasicCredential(string.Empty, pat));
+            witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+            this.projectId = projectId;
+        }
+
+        public async Task AddPBITasks(int pbiId)
+        {
+            try
+            {
+                var pbi = await witClient.GetWorkItemAsync(pbiId);
+                await CreateChildTask(pbi, "Do something...");
+                await CreateChildTask(pbi, "Do something more...");
+            }
+            catch (Exception ex)
+            {
+                // who cares! ;-)
+            }
+        }
+
+        private async Task<WorkItem> CreateChildTask(Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItem parentPbi, string title)
+        {
+            var document = new JsonPatchDocument();
+            document.Add(
+                new JsonPatchOperation()
+                {
+                    Path = "/fields/System.Title",
+                    Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
+                    Value = title
+                });
+
+            document.Add(
+                new JsonPatchOperation()
+                {
+                    Path = "/relations/-",
+                    Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
+                    Value = new
+                    {
+                        rel = "System.LinkTypes.Hierarchy-Reverse",
+                        url = parentPbi.Url
+                    }
+                    //https://fabrikam-fiber-inc.visualstudio.com/DefaultCollection/_apis/wit/workItems/297
+                });
+
+            return await witClient.CreateWorkItemAsync(document, projectId, "Task");
+        }
+
+        public void Dispose()
+        {
+            witClient.Dispose();
+            connection.Disconnect();
+            witClient = null;
+            connection = null;
+        }
+    }
+}
+
+
---
+++ DemoWorkItemEventHandler/WebHooks/VstsWebHookHandlercs.cs
@@ -0,0 +1,35 @@
+﻿using DemoWorkItemEventHandler.VSTSClient;
+using Microsoft.AspNet.WebHooks;
+using Microsoft.AspNet.WebHooks.Payloads;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DemoWorkItemEventHandler.WebHooks
+{
+    public class VstsWebHookHandlercs : VstsWebHookHandlerBase
+    {
+        public async override Task ExecuteAsync(WebHookHandlerContext context,
+                                                    WorkItemUpdatedPayload payload)
+        {
+            if (payload.Resource.Fields.SystemState.OldValue == "New" && // sample check
+                payload.Resource.Fields.SystemState.NewValue == "Approved")
+            {
+                // your logic goes here...
+               
+                var projectId = Guid.Parse(payload.ResourceContainers.Project.Id);
+                var wiId = payload.Resource.WorkItemId;
+                var pat = ConfigurationManager.AppSettings.Get("VSTS_PAT");
+                var vstsurl = ConfigurationManager.AppSettings.Get("VSTS_URL");
+
+                using (var service = new WorkItemService(vstsurl, projectId, pat))
+                {
+                    await service.AddPBITasks(wiId);
+                }
+            }
+        }
+    }
+}
